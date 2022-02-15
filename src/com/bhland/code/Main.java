bd10f7abc86e1b56da46dda80ce0c78fef27874a
Sunaada Hebbar <shebbarm@asu.edu>
2022-02-14 19:29:42 -0700

    - added resource folder and video file - added template code

--- src/com/bhland/code/Main.java
+++ src/com/bhland/code/Main.java
@@ -1,8 +1,14 @@
 package com.bhland.code;
 
+import java.io.File;
+
 public class Main {
 
+    private final static String VIDEO_FILE_PATH = "../res/FingerTipVideo.mp4";
+
     public static void main(String[] args) {
-        System.out.println("Test Main");
+        File videoFile = new File(VIDEO_FILE_PATH);
+        int heartRate = MeasureHeartRateUtility.ComputeHeartRate(videoFile);
+        System.out.println("The measure heart rate is: " + heartRate);
     }
 }
---
+++ src/com/bhland/code/MeasureHeartRateUtility.java
@@ -0,0 +1,10 @@
+package com.bhland.code;
+
+import java.io.File;
+
+public class MeasureHeartRateUtility {
+    public static int ComputeHeartRate(File videoFile) {
+        // TODO
+        return 0;
+    }
+}
